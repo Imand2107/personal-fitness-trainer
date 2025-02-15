2801c63316d880ffab8c53daa273085a7e65b026
Iman Daniel <imand2107@gmail.com>
2025-02-15 15:23:43 +0800

    Update background color in workout session to use defined color constant

--- app/(tabs)/workout/session/[id].tsx
+++ app/(tabs)/workout/session/[id].tsx
@@ -794,7 +794,7 @@ const styles = StyleSheet.create({
     left: 0,
     right: 0,
     bottom: 0,
-    backgroundColor: 'rgba(0,0,0,0.7)',
+    backgroundColor: COLORS.background,
     justifyContent: 'center',
     alignItems: 'center',
     zIndex: 1000,
